5382261ec109db93bb18d74b15c109ba7985f35e
Franklin Jimenez <frank@udenar.edu.co>
2018-04-07 17:55:03 -0500

    edicion eliminacion y creacion ok

--- album/views.py
+++ album/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from album.models import Category, Photo
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy 
+from django.views.generic import CreateView, DeleteView, UpdateView, ListView, DetailView
 
 # Create your views here.
 def first_view(request):
@@ -24,3 +25,15 @@ class PhotoListView(ListView):
 
 class PhotoDetailView(DetailView):
     model = Photo
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = '__all__' 
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = '__all__'
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    success_url = reverse_lazy('photo-list')
